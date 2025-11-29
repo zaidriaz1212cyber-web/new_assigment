a71869e26d30e519e30cda3a83dc1247f9b4339d
zaid <zaidriaz1212cyber@gmail.com>
2025-11-29 15:13:58 +0500

    new assignment

---
+++ app.js
@@ -0,0 +1,16 @@
+//Legal and illegal variables name in javaScript
+
+//Legal variable names
+var_name = "zaid"
+_varname =  "zaid"
+varname = "zaid"
+varname123 = "zaid"
+$carname = "zaid"
+_var_name = "zaid"
+
+//(Illegal variable names
+//only underscorse(_),numbers,digit and dollar sign($) are allowed in variable names
+//var-name = "zaid" //hyphen is not allowed
+//var name = "zaid" //space is not allowed
+//123varname = "zaid" //variable name cannot start with a number
+//var@name = "zaid" //special characters are not allowed except $ and _ )
